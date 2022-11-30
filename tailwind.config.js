7cdb0b57db4ebb07770e542b4280883689cbde24
”unChris666” <christian.121450112@student.itera.ac.id>
2022-11-30 14:55:35 +0700

    pertama

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
