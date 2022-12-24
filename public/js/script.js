5b7e474dd5886b6ed89d1a98d8d47c6a9d8b876c
”unChris666” <christian.121450112@student.itera.ac.id>
2022-12-25 00:06:18 +0700

    "fisrt commit after restart"

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,31 @@
-// Get the container element
-const btn = document.querySelector('button.mobile-menu-button');
-const menu = document.querySelector('.mobile-menu');
+//navbar-fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixednav = header.offsetTop;
+  const toTop = document.querySelector("#toTop");
 
-btn.addEventListener('click', () =>{
-  menu.classList.toggle('hidden');
-})
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+//hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
